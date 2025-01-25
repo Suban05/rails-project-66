a7176a55fbdf3d2e4b1f70f32f31375143f88e16
Suban05 <anatolyb94@gmail.com>
2025-01-26 01:22:31 +0500

    add model `repositories`

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_250_126_003_907) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'nickname'
-    t.string 'token'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_003908) do
+  create_table "repositories", force: :cascade do |t|
+    t.string "name"
+    t.integer "github_id", null: false
+    t.string "full_name"
+    t.string "language"
+    t.string "clone_url"
+    t.string "ssh_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_repositories_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "nickname"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "repositories", "users"
 end
