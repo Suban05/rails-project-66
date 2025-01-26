bbd4f803f02b9337686ecb7df74def2c2bb05845
Suban05 <anatolyb94@gmail.com>
2025-01-26 20:33:46 +0500

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   scope module: :web do
     root 'home#index'
 
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     delete 'auth/logout', to: 'auth#logout'
+
+    resources :repositories, only: %i[index show new create] do
+      scope module: :repositories do
+        resources :checks, only: %i[create show]
+      end
+    end
   end
 
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  namespace :api do
+    resources :checks, only: %i[create]
+  end
 end
