93ccd34fc576c329e8adb24ca7f71bd73f8ba39e
Suban05 <anatolyb94@gmail.com>
2025-01-26 20:57:21 +0500

    schema up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_26_003908) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_131714) do
   create_table "repositories", force: :cascade do |t|
     t.string "name"
     t.integer "github_id", null: false
@@ -24,14 +24,28 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_26_003908) do
     t.index ["user_id"], name: "index_repositories_on_user_id"
   end
 
+  create_table "repository_checks", force: :cascade do |t|
+    t.integer "repository_id", null: false
+    t.string "aasm_state", default: "created", null: false
+    t.string "commit_id"
+    t.boolean "passed", default: false
+    t.json "linter_check_result", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["repository_id"], name: "index_repository_checks_on_repository_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "nickname"
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "repositories", "users"
+  add_foreign_key "repository_checks", "repositories"
 end
