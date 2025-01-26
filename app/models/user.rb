b866edd39239510c45302e816f62fc04f43f179a
Suban05 <anatolyb94@gmail.com>
2025-01-26 20:53:42 +0500

    user has many repos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  has_many :repositories, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
